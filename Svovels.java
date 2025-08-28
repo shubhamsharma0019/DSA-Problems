cc696689a73e2094ef8bb2b2ffd48d1864b40869
shubham sharma <shubhamsharma639615@gmail.com>
2025-08-29 04:39:57 +0530

    first commit

---
+++ Svovels.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Svovels {
+    public static void main(String[] args){
+       Scanner scn = new Scanner(System.in);
+       String str = scn.nextLine();
+       
+       int count = 0;
+       str = str.toLowerCase();
+       for(int i=0;i < str.length();i++){
+        char ch = str.charAt(i);
+        if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'){
+           count++;
+        }
+
+       }
+       System.out.print(count);
+    }
+    
+}
