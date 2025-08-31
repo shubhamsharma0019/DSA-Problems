fda7e2da5d23be6664c5b7b243bdf3f2515d5617
shubham sharma <shubhamsharma639615@gmail.com>
2025-09-01 05:26:40 +0530

    count

---
+++ ScountCh.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ScountCh {
+   public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Eneter a String: ");
+    String str = scn.nextLine();
+
+    System.out.println("Enter a character to count: ");
+    char ch = scn.next().charAt(0);
+
+    int count = 0;
+
+    for(int i=0;i<str.length();i++){
+        if(str.charAt(i)== ch){
+            count++;
+        }
+    }
+    System.out.println("Character appears "+ count+" times.");
+
+
+   }
+}
