11aa2b4e2a30281eb14c7786ab8b8b75779e998f
shubham sharma <shubhamsharma639615@gmail.com>
2025-09-04 06:44:49 +0530

    04sep

---
+++ Amax.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Amax {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int []arr = new int [n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = scn.nextInt();
+        }
+        int max = arr[0];
+        for(int i =0;i<n;i++){
+            if(arr[i]>=max){
+              max = arr[i];
+            }
+        }
+        System.out.println(max);
+        
+    }
+    
+}
