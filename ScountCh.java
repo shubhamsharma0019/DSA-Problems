64389ec6badb5f6ae32d113c39ce9ce19f7e2d5c
shubham sharma <shubhamsharma639615@gmail.com>
2025-09-04 06:37:24 +0530

    04

--- ScountCh.java
+++ ScountCh.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ScountCh {
    public static void main(String[] args){
     Scanner scn = new Scanner(System.in);
-    System.out.println("Eneter a String: ");
+    System.out.println("Enter a String: ");
     String str = scn.nextLine();
 
     System.out.println("Enter a character to count: ");
@@ -11,13 +11,11 @@ public class ScountCh {
 
     int count = 0;
 
-    for(int i=0;i<str.length();i++){
+    for(int i=0;i<str.length();i++){ 
         if(str.charAt(i)== ch){
             count++;
         }
     }
-    System.out.println("Character appears "+ count+" times.");
-
-
+    System.out.println("Character appears "+ count +" times.");
    }
 }
