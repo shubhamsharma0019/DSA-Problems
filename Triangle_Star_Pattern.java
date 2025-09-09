1f5fbcc430c5a86a6c2de13651547856f7d7a46b
shubham sharma <shubhamsharma639615@gmail.com>
2025-09-09 06:58:31 +0530

    09sep

---
+++ Triangle_Star_Pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Triangle_Star_Pattern {
+     public static void main(String[]args){
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the numbers:");
+        int n = scn.nextInt();
+        int nst = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=nst;j++){
+                System.out.print("* ");
+            }
+            nst++;
+            System.out.println();
+        }
+     }
+    
+}
